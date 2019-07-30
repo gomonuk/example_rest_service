6ac3345dd8196288dc576c2d561e2cc4d9fd3f5d
Даниил Гомонюк <d.gomonyuk@realweb.ru>
2019-07-30 14:25:05 +0300

    add trigger

--- src/table_operations.py
+++ src/table_operations.py
@@ -32,67 +32,73 @@ class TableOperations(object):
             database="postgres",
             user="postgres",
             password="",
-            host="0.0.0.0",
-            # host="postgres",
+            # host="0.0.0.0",
+            host="postgres",
             port="5432",
         )
         await self.__setup_database()
 
     async def __setup_database(self):
         sql_sqripts = [
-            "DROP TABLE IF EXISTS logs",
-            "DROP TABLE IF EXISTS account",
-            "DROP TABLE IF EXISTS joke",
-            """CREATE OR REPLACE FUNCTION random_between(low INT ,high INT) RETURNS INT AS
-                              $$
-                              BEGIN
-                                 RETURN floor(random()* (high-low + 1) + low);
-                              END;
-                              $$ language 'plpgsql' STRICT;
-                              """,
-            """create table account
-                              (
-                                  id    serial                                               not null
-                                      constraint account_pk
-                                          primary key,
-                                  login text                                                 not null,
-                                  key   integer default random_between(100000000, 999999999) not null,
-                                  jokes integer[]
-                              );
-                              alter table account
-                                  owner to postgres;
-                              create unique index account_key_uindex
-                                  on account (key);
-                                    """,
-            """create table joke
-                              (
-                                  id   serial not null
-                                      constraint joke_pk
-                                          primary key,
-                                  text text default ''::text,
-                                  number_of_uses int default 1
-                              );
-                              alter table joke
-                                  owner to postgres;
-                              create unique index joke_text_uindex
-                                  on joke (text);
-                                  """,
+            """DROP TABLE IF EXISTS logs""",
+            """DROP TABLE IF EXISTS account""",
+            """DROP TABLE IF EXISTS joke""",
+            """create table account (
+                   id    serial                                               not null
+                       constraint account_pk
+                           primary key,
+                   login text                                                 not null,
+                   key   integer default random_between(100000000, 999999999) not null,
+                   jokes integer[]
+               );
+               alter table account
+                   owner to postgres;
+               create unique index account_key_uindex
+                   on account (key);
+                     """,
+            """create table joke (
+                   id   serial not null
+                       constraint joke_pk
+                           primary key,
+                   text text default ''::text,
+                   number_of_uses int default 1
+               );
+               alter table joke
+                   owner to postgres;
+               create unique index joke_text_uindex
+                   on joke (text);
+                   """,
             """create table logs (
-                                key          integer,
-                                ip_address   text,
-                                request_time timestamp default now(),
-                                id           serial not null
-                                  constraint logs_pk
-                                    primary key
-                              );
-                              alter table logs
-                                owner to postgres;
-                              """,
+                 key          integer,
+                 ip_address   text,
+                 request_time timestamp default now(),
+                 id           serial not null
+                   constraint logs_pk
+                     primary key
+               );
+               alter table logs
+                 owner to postgres;
+               """,
             """CREATE OR REPLACE FUNCTION array_unique (ANYARRAY) RETURNS ANYARRAY
-                              LANGUAGE SQL
-                              AS $body$
-                                SELECT array(SELECT DISTINCT UNNEST($1)) ;
-                              $body$;""",
+               LANGUAGE SQL
+               AS $body$
+                 SELECT array(SELECT DISTINCT UNNEST($1)) ;
+               $body$;""",
+            """CREATE OR REPLACE FUNCTION remove_joke() RETURNS TRIGGER AS $$
+               BEGIN
+                  IF  NEW.number_of_uses = 0  THEN
+                  DELETE FROM joke WHERE id = NEW.id;
+                  RETURN NEW;
+                  END IF;
+               END;
+               $$ LANGUAGE plpgsql;""",
+            """CREATE TRIGGER remove_joke AFTER UPDATE ON joke FOR EACH ROW EXECUTE PROCEDURE remove_joke();""",
+            """CREATE OR REPLACE FUNCTION random_between(low INT ,high INT) RETURNS INT AS
+               $$
+               BEGIN
+                  RETURN floor(random()* (high-low + 1) + low);
+               END;
+               $$ language 'plpgsql' STRICT;""",
         ]
         async with self.engine.acquire() as conn:
             for script in sql_sqripts:
